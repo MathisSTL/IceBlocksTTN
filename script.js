149a4a6f477016719eed4fb1c1d7eb4f9d98944d
MathisSTL <141674101+MathisSTL@users.noreply.github.com>
2024-08-03 11:51:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateAnswer() {
     var userInput = document.getElementById("userInput").value;
     var message = document.getElementById("message");
     
-    if (userInput.trim().toLowerCase() === "onze") {
+    if (userInput.trim().toLowerCase() === "54") {
         message.innerHTML = '<a href="https://mathisstl.github.io/RGB/" target="_blank">https://mathisstl.github.io/RGB/</a>';
         message.style.color = "green";
     } else {
